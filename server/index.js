f1835f7a8922d02c50284a553fc649e28a3fbe4b
Marcy <marcy@Marcys-MBP-4.home>
2024-09-24 18:08:27 -0400

    finished

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const API_KEY = process.env.API_KEY;
+const path = require('path');
+const pathToDist = path.join(__dirname, '../giphy-search/dist')
+const serveStatic = express.static(pathToDist)
+
+const serveGifs = async (req, res, next) => {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
+    const data = await response.json()
+    res.send(data);
+}
+app.use(serveStatic)
+app.get("/api/gifs", serveGifs);
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+})
