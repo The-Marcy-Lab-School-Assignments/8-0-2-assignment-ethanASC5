4d65223131ac350e2eca349148a5bae3fd16b880
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-10-16 14:17:56 -0400

    feedback

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.API_KEY;
 const path = require('path');
 const pathToDist = path.join(__dirname, '../giphy-search/dist')
 const serveStatic = express.static(pathToDist)
-
+/** FEEDBACK: Great job!  */
 const serveGifs = async (req, res, next) => {
     const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
     const data = await response.json()
